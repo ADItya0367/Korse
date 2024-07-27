c5fc33b8bec58fd21aacf3e2cc55416260e41476
ADItya0367 <21egjcs001@gitjaipur.com>
2024-07-27 23:33:00 +0530

    Second Build

--- korse/src/App.js
+++ korse/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes,Route, Switch} from 'react-router-dom';
+import Home from './pages/Home';
+import Offers from './pages/Offers';
+import ForgotPassword from './pages/ForgotPassword';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/offers' element={<Offers/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/signin' element={<SignIn/>}/>
+          <Route path='/forgotpassword' element={<ForgotPassword/>}/>
+          </Routes>
+      </Router>
     </div>
   );
 }
---
+++ korse/src/pages/Offers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Offers() {
+  return (
+    <div>
+        <h1>Offers</h1>
+    </div>
+  )
+}
